d5afb03efac2ee58f6267511891032cf9b23c233
Chaman Gowda P <chaman.p@s.amity.edu>
2025-06-30 16:31:08 +0530

    add

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	double num1,num2;
+	char operator;
+	double result;
+
+	printf("Enter first number: ");
+	scanf("%lf",&num1);
+
+	printf("Enter a operator(+,-,*,/): ");
+	scanf("%c", &operator);
+
+	printf("Enter second number: ");
+	scanf("%lf",&num2);
+
+	switch(operator){
+		case'+':
+			result=num1+num2;
+			printf("result: %.2lf\n",result);
+			break;
+
